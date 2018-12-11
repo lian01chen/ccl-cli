86c9b608994f9e7c969ddd8b3a92ab25c6410c98
lian01.chen <chenlian619@163.com>
2018-12-11 20:47:19 +0800

    feat:init ccl-cli

---
+++ bin/ccl-cli.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+const download = require('download-git-repo') // 下载模版
+const program = require('commander') // 命令处理
+const chalk = require('chalk') // 打印字体颜色
+const ora = require('ora')  // 设置
+const fs = require('fs')
+
+const { message, isEmptyDir, abort } = require('../common/util')
+
+// 初始化一个项目
+program
+  .command('init <projectName>')
+  .alias('i')
+  .description('init a new template project')
+  .action((projectName)=>{
+    loadTemplate(projectName)
+  })
+program.parse(process.argv)
+
+/**
+ * 下载项目模板
+ */
+function loadTemplate(dir){
+  // 查看是否已经有同名的项目了？
+  if(fs.existsSync(dir) && isEmptyDir(dir)){
+    abort('dir exist!') 
+  }
+  let spinner = ora('Loading rainbows ...').start()
+  download('lian01chen/my-react-demo', dir, function (err) {
+    spinner.stop()
+    err ? message.error('fail') : message.success('success')
+  })
+}
---
+++ common/util.js
@@ -0,0 +1,40 @@
+const chalk = require('chalk')
+const fs = require('fs')
+
+const message = {
+  success(text){
+    console.log(chalk.green.bold(text))
+  },
+  error (text) {
+    console.log(chalk.red.bold(text));
+  },
+  info (text) {
+    console.log(chalk.blue.bold(text));
+  },
+  light (text) {
+    console.log(chalk.yellow.bold(text));
+  }
+}
+/**
+ * node 异常，终止
+ * @param {*} msg 
+ */
+function abort(msg){
+  message.error(msg)
+  process.exit(1)
+}
+
+/**
+ * 判断一个目录是否是空目录
+ * @param {*} dir 
+ */
+function isEmptyDir(dir){
+  let files = fs.readdirSync(dir)
+  return files.length ? true : false
+}
+
+module.exports = {
+  message,
+  abort,
+  isEmptyDir
+}
