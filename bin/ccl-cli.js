497c39bf98695e089b1347a75effca3e7650cfd1
lian01.chen <chenlian619@163.com>
2019-01-15 14:36:49 +0800

    feat:command check

--- bin/ccl-cli.js
+++ bin/ccl-cli.js
@@ -3,7 +3,7 @@
 const download = require('download-git-repo') // 下载模版
 const program = require('commander') // 命令处理
 const chalk = require('chalk') // 打印字体颜色
-const ora = require('ora')  // 设置
+const ora = require('ora') // 设置
 const fs = require('fs')
 
 const { message, isEmptyDir, abort } = require('../common/util')
@@ -13,21 +13,27 @@ program
   .command('init <projectName>')
   .alias('i')
   .description('init a new template project')
-  .action((projectName)=>{
+  .action(projectName => {
     loadTemplate(projectName)
   })
+program.command('*').action(() => {
+  program.help()
+})
 program.parse(process.argv)
 
+if (program.args.length < 1) {
+  program.help()
+}
 /**
  * 下载项目模板
  */
-function loadTemplate(dir){
+function loadTemplate(dir) {
   // 查看是否已经有同名的项目了？
-  if(fs.existsSync(dir) && isEmptyDir(dir)){
-    abort('dir exist!') 
+  if (fs.existsSync(dir) && isEmptyDir(dir)) {
+    abort('dir exist!')
   }
   let spinner = ora('Loading rainbows ...').start()
-  download('lian01chen/ccl-base', dir, function (err) {
+  download('lian01chen/ccl-base', dir, function(err) {
     spinner.stop()
     err ? message.error('fail') : message.success('success')
   })
